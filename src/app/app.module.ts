b547437cfec089f86c35efcfd650302360e57f1b
Antonio Carlos Guimarães de Faria Junior <guimas244@gmail.com>
2020-11-08 13:28:57 -0300

    adiciona configuracao firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,29 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyCm22yOn43z0Iad5ZsJ_8kJo-j6ap310ng",
+      authDomain: "trabalho-ionic-9c54a.firebaseapp.com",
+      databaseURL: "https://trabalho-ionic-9c54a.firebaseio.com",
+      projectId: "trabalho-ionic-9c54a",
+      storageBucket: "trabalho-ionic-9c54a.appspot.com",
+      messagingSenderId: "1019187403437",
+      appId: "1:1019187403437:web:89b23d447e4349bae58424",
+      measurementId: "G-PJDDGJN62H"
+    }),
+    AngularFireDatabaseModule
+
+  ],
   providers: [
     StatusBar,
     SplashScreen,
@@ -20,4 +38,4 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
